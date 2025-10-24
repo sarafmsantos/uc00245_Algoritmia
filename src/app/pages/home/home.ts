560219ff1e12e53ab1662adfc7b91d076fc81800
sarafmsantos <sarafmsantos@gmail.com>
2025-10-24 19:00:39 +0100

    Exercicios

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -10,13 +10,16 @@ export class Home implements OnInit {
 
 variavelGlobal: string = "";
 variavelGlobal2!: string;
+variavelGlobal3: number = 0;
+arrayGlobal: string[] = [];
 
   ngOnInit(): void {
     let teste = '';
     const nome = '';
-
+    
     this.exemploVariaveisETipos();
     this.exemploVariaveisEtipos2();
+    this.exerciciosparafazer();
   }
 
 // ======================================================
@@ -70,4 +73,68 @@ exemploOperadoresAritmeticos(): void {
   console.log(num1 / num2); // Divisão
   console.log(num1 % num2); // Resto da divisão
 }
+/***************************Exercícios *********************************/
+
+exerciciosparafazer = () =>{
+        // 💪 Exercício 1
+    console.log(
+      '👉 Exercício 1: Declare 3 variáveis: nome (string), idade (number) e ativo (boolean). Mostre-as no console.'
+    );
+    let nome = "sara";
+    let idade = 43;
+    let ativo = false;
+
+    console.log(nome);
+    console.log(idade);
+    console.log(ativo);
+
+        // 💪 Exercício 2
+    console.log(
+      '👉 Exercício 2: Crie uma variável cidade e depois altere seu valor.'
+    );
+    let cidade = "Setúbal";
+    console.log(cidade);
+
+    cidade = "Torrão";
+    console.log(cidade);
+
+        // 💪 Exercício 3
+    console.log(
+      '👉 Exercício 3: Crie uma variável do tipo any e mude seu tipo três vezes.'
+    );
+    let valor: any = 0;
+    console.log(valor);
+    valor = "sara";
+    console.log(valor);
+    valor = false;
+    console.log(valor);
+
+        // 💪 Exercício 4
+    console.log(
+      '👉 Exercício 4: Crie duas variáveis e mostre no console a soma e subtração delas.'
+    );
+      let num1 = 23;
+      console.log(num1);
+      let num2 = 78;
+      console.log(num2);
+      console.log(num1 + num2);
+      console.log(num2 - num1);
+
+        // 💪 Exercício 5
+    console.log(
+      '👉 Exercício 5: Calcule a área de um retângulo (largura * altura).'
+    );
+      let largura = 30;
+      console.log(largura);
+      let altura = 20;
+      console.log(altura);
+      let area = largura * altura;
+      console.log(area);
+
+    // 💪 Exercício 6
+    console.log('👉 Exercício 6: Mostre o resto da divisão de 20 por 6.'
+    );
+    let resto = 20 % 6;
+    console.log(resto);
+  }
 }
