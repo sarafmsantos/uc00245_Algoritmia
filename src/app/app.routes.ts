0bdb0a6684db78ccb85728cb6a329ddb9a86d636
sarafmsantos <sarafmsantos@gmail.com>
2025-10-30 00:17:00 +0000

    nova correção

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { aula02 } from './pages/aula02/aula02';
-import { aula01 } from './pages/aula01/Aula01';
+import { aula01 } from './pages/aula01/aula01';
  
 export const routes: Routes = [
   { path: '', redirectTo: 'aula02', pathMatch: 'full' },
